740977303cb307d8099c7659175c2e9bd2b6185b
Saroj Parida <SarojP@cogcis.onmicrosoft.com>
2018-06-08 06:12:58 +0000

    Other Templates Iteams

---
+++ HackathonTemplates/Other Templates/SQL DB Create.ps1
@@ -0,0 +1,6 @@
+﻿Login-AzureRmAccount
+
+$resourcegroupname = "hackathonteamdbrg001"
+$servername = "hackathonsqlserver001"
+$databasename = "hackathonteam325_db"
+New-AzureRmSqlDatabase -ResourceGroupName $resourcegroupname -ServerName $servername -DatabaseName $databasename -Edition Basic
